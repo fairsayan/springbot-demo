609af21268d400330bd8e130dc1a3ff596c7f14c
Domenico Pontari <fairsayan@gmail.com>
2019-12-23 15:12:26 +0100

    deploy commit

--- src/main/java/by/wink/springbot/SpringbotApplication.java
+++ src/main/java/by/wink/springbot/SpringbotApplication.java
@@ -3,6 +3,7 @@ package by.wink.springbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringbotApplication {
 
